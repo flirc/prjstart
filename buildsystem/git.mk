50c1c1d6d0064e99d71e7b1b399c9d02fac15eb4
Robert Curtis <robert.caston@gmail.com>
2012-10-17 18:41:46 -0700

    make git version cleaner for untagged repositories

--- buildsystem/git.mk
+++ buildsystem/git.mk
@@ -1,5 +1,4 @@
-SCMVERSION := $(shell git describe --tags --dirty=+ 2>/dev/null || \
-		git rev-parse --short=16 HEAD 2>/dev/null || \
+SCMVERSION := $(shell git describe --tags --dirty=+ --always 2>/dev/null || \
 		echo "UNKNOWN")
 SCMBRANCH := $(shell (git branch 2> /dev/null || echo '* UNKNOWN') | \
 		awk '/^\* / {print $$2;}')
